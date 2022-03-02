7f85ee8dfd3d012c11d6a212c3e41b8bf5a02597
nrvogel543 <nrvogel@loyola.edu>
2022-03-02 17:51:08 -0500

    Finished Form

---
+++ AddQuestion.js
@@ -0,0 +1,10 @@
+//submitForm();
+
+function submitForm()
+{
+    document.getElementById('QuestionForm').style.display = "none";
+
+    document.getElementById("FormSubmitted").style.display = "block";
+
+    console.log('Form Submitted');
+}
