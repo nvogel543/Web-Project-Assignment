463891f91d204f38d62adfe6e79fcd72e2260cca
nrvogel543 <nrvogel@loyola.edu>
2022-03-25 00:24:21 -0400

    Removed unnecessary function

--- NicksTrivia.js
+++ NicksTrivia.js
@@ -19,19 +19,3 @@ app.get('/question',function(req,res,next){
     res.send(questions);
     res.end();
  });
- 
-const readQuestions = async function(){
-  let out = `<table class="table table-striped table-hover">
-          <thead><tr><th>ID</th><th>Login</th></tr><\thead>`;
-  for(let i=0; i<questions.length; i++){
-     out += `<tr> <td>${questions[i]._id}</td>
-             <td>${questions[i].Topic}</td>
-             <td>${questions[i].Question}</td>
-             <td>${questions[i].Answer}</td>
-             <td>${questions[i].WrongAnswer1}</td>
-             <td>${questions[i].WrongAnswer2}</td>
-             <td>${questions[i].WrongAnswer3}</td></tr>`;
-  }
-  out += `</table>`; 
-  document.getElementById('QuestionData').innerHTML = out;
-}
