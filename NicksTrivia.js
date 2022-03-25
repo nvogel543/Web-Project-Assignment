1280a6288e7102479e23237f1a67e70638903e79
nrvogel543 <nrvogel@loyola.edu>
2022-03-24 21:16:21 -0400

    Created Questions.json and get method

--- NicksTrivia.js
+++ NicksTrivia.js
@@ -10,3 +10,13 @@ app.use(express.static('public'));
 const server=app.listen(port,hostname,function(){
    console.log(`Server running in ${hostname}:${port}`);
 });
+
+let QuestionsJSON = require('./Questions.json');
+let questions = QuestionsJSON.questions;
+
+app.get('/question',function(req,res,next){
+    res.status(200);
+    res.send(questions);
+    res.end();
+ });
+ 
